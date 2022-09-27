122c93222bca135608861b42cb17b4e9c2b16b38
web-scape <webscape.webdevs@gmail.com>
2022-09-27 22:17:44 +0530

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import React from "react";
-import Home from "../src/layout/Home/Home";
+import Home from "../src/containers/Home/Home";
 
 function App() {
   return (
